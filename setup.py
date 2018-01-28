1b674da54d85be48f602e9f28b7b83358e6b7934
Igor <igor955@gmail.com>
2018-01-28 23:16:36 +0100

    setup.py edit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='FacebookPostsAnalysis',
-    version='0.1',
+    version='0.1.2',
     description='Application to analyze the posts of a Facebook page/group.',
     long_description=long_description,
     author='Igor Rosocha',
