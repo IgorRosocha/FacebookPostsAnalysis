afedb21b85f2787ff75d0b7b5a94e78e09db5b9e
Igor <igor955@gmail.com>
2018-01-31 17:43:18 +0100

    modules

--- FacebookPostsAnalysis/analysis.py
+++ FacebookPostsAnalysis/analysis.py
@@ -5,6 +5,7 @@ import datetime
 import csv
 import nbformat as nbf
 import time
+from FacebookPostsAnalysis.notebook import create_notebook
 
 
 def create_request(url, session):
@@ -157,129 +158,129 @@ def get_reactions(url, session):
     return reactions_count
 
 
-def create_notebook(entity_id):
-    """
-    Create a Jupyter Notebook, containing the full analysis of Facebook group/page.
-
-    :param entity_id: ID of Facebook group/page
-    """
-    nb = nbf.v4.new_notebook()
-    header = '# Facebook Posts Analysis\n' \
-             'Analysis based on data gathered from Facebook page/group.\n\n' \
-             'For more information, please check the documentation or ' \
-             'visit https://github.com/IgorRosocha/FacebookPostsAnalysis\n\n' \
-             'ID of analyzed group/page: {}'.format(entity_id)
-
-    images = '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-             'analysis/static/images/Python.png?raw=true" style="width: 100px; float: left;"/>\n\n' \
-             '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-             'analysis/static/images/Facebook.png?raw=true" style="width: 100px; float: left;"/>\n\n' \
-             '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-             'analysis/static/images/Pandas.png?raw=true" style="width: 400px; float: left;"/>'
-
-    imports = 'import pandas as pd\n' \
-              'import matplotlib\n' \
-              'import numpy as np\n' \
-              'import calendar\n\n' \
-              '%matplotlib inline'
-
-    number_header = '### 1. Total number of posts:'
-    number_of_posts = 'results = pd.read_csv("analysis_csv_{}.csv", index_col=None)\n'.format(entity_id) + \
-                      'number_of_posts = results["ID"].count()\n' \
-                      'print("Total number of posts: {}".format(number_of_posts))'
-
-    popular_header = '### 2. Most popular posts:'
-    popular_posts = 'def find_most(column_name):\n' \
-                    '    most = results[["ID", column_name, "Author", "Date created", "Message"]]\n' \
-                    '    most = most.sort_values(by=[column_name], ascending=False)[:5].reset_index(drop=True)\n' \
-                    '    most.index = most.index + 1\n' \
-                    '    return most'
-
-    reactions_subheader = '**a) Posts with the highest number of reactions:**\n\n' \
-                          '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-                          'analysis/static/images/Reactions.png?raw=true" style="width: 400px; float: left;"/>'
-    most_reactions = 'find_most("Number of reactions")'
-
-    likes_subheader = '**b) Posts with the highest number of likes:**\n\n' \
-                      '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-                      'analysis/static/images/Like.png?raw=true" style="width: 400px; float: left;"/>'
-    most_likes = 'find_most("Number of Likes")'
-
-    shares_subheader = '**c) Posts with the highest number of shares:**\n\n' \
-                       '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-                       'analysis/static/images/Share.png?raw=true" style="width: 400px; float: left;"/>'
-    most_shares = 'find_most("Number of shares")'
-
-    comments_subheader = '**d) Posts with the highest number of comments:**\n\n' \
-                         '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-                         'analysis/static/images/Comment.png?raw=true" style="width: 400px; float: left;"/>'
-    most_comments = 'find_most("Number of comments")'
-
-    unpopular_header = '### 2. Most unpopular posts:'
-
-    angrys_subheader = '**a) Posts with the highest number of ANGRY reactions:**\n\n' \
-                       '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-                       'analysis/static/images/Angry.png?raw=true" style="width: 400px; float: left;"/>'
-    most_angrys = 'find_most("Number of Angrys")'
-
-    sads_subheader = '**b) Posts with the highest number of SAD reactions:**\n\n' \
-                     '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
-                     'analysis/static/images/Sad.png?raw=true" style="width: 400px; float: left;"/>'
-    most_sads = 'find_most("Number of Sads")'
-
-    months_header = '### 3. Months with the biggest activity'
-
-    biggest_activity = 'activity = results[["Number of reactions", "Number of comments", "Date created"]]\n' \
-                       'activity = activity.assign(Ratio=activity["Number of reactions"] + ' \
-                       'activity["Number of comments"])\n' \
-                       'activity["Date created"] = pd.to_datetime(activity["Date created"])\n' \
-                       'activity = activity.sort_values(by="Date created")\n' \
-                       'activity["mnth_yr"] = activity["Date created"].apply(lambda x: x.strftime("%b-%Y"))\n' \
-                       'activity = activity.groupby(["mnth_yr"], sort=False).sum()\n' \
-                       'activity = activity.drop(["Number of comments", "Number of reactions"], 1)'
-
-    graph = 'if activity["Ratio"].max() < 250:\n' \
-            '    ticks = 10\n' \
-            'elif activity["Ratio"].max() < 2500:\n' \
-            '    ticks = 100\n' \
-            'else:\n' \
-            '    ticks = 1000\n\n' \
-            'graph = activity.plot.bar(color="#8b9dc3", title="Months with the biggest activity", ' \
-            'figsize=(15,5), legend=True)\n' \
-            'graph.set_xlabel("Month")\n' \
-            'graph.set_ylabel("Activity Ratio")\n' \
-            'graph.yaxis.set_ticks(np.arange(0,activity["Ratio"].max(),ticks))\n' \
-            'graph.set_facecolor("#f7f7f7")\n' \
-            'graph.grid("on", which="major", axis="y")'
-
-    nb['cells'] = [nbf.v4.new_markdown_cell(header),
-                   nbf.v4.new_markdown_cell(images),
-                   nbf.v4.new_code_cell(imports),
-                   nbf.v4.new_markdown_cell(number_header),
-                   nbf.v4.new_code_cell(number_of_posts),
-                   nbf.v4.new_markdown_cell(popular_header),
-                   nbf.v4.new_code_cell(popular_posts),
-                   nbf.v4.new_markdown_cell(reactions_subheader),
-                   nbf.v4.new_code_cell(most_reactions),
-                   nbf.v4.new_markdown_cell(likes_subheader),
-                   nbf.v4.new_code_cell(most_likes),
-                   nbf.v4.new_markdown_cell(shares_subheader),
-                   nbf.v4.new_code_cell(most_shares),
-                   nbf.v4.new_markdown_cell(comments_subheader),
-                   nbf.v4.new_code_cell(most_comments),
-                   nbf.v4.new_markdown_cell(unpopular_header),
-                   nbf.v4.new_markdown_cell(angrys_subheader),
-                   nbf.v4.new_code_cell(most_angrys),
-                   nbf.v4.new_markdown_cell(sads_subheader),
-                   nbf.v4.new_code_cell(most_sads),
-                   nbf.v4.new_markdown_cell(months_header),
-                   nbf.v4.new_code_cell(biggest_activity),
-                   nbf.v4.new_code_cell(graph)]
-
-    notebook_name = 'analysis_' + entity_id + '.ipynb'
-
-    with open(notebook_name, 'w') as f:
-        nbf.write(nb, f)
+# def create_notebook(entity_id):
+#     """
+#     Create a Jupyter Notebook, containing the full analysis of Facebook group/page.
+#
+#     :param entity_id: ID of Facebook group/page
+#     """
+#     nb = nbf.v4.new_notebook()
+#     header = '# Facebook Posts Analysis\n' \
+#              'Analysis based on data gathered from Facebook page/group.\n\n' \
+#              'For more information, please check the documentation or ' \
+#              'visit https://github.com/IgorRosocha/FacebookPostsAnalysis\n\n' \
+#              'ID of analyzed group/page: {}'.format(entity_id)
+#
+#     images = '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#              'analysis/static/images/Python.png?raw=true" style="width: 100px; float: left;"/>\n\n' \
+#              '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#              'analysis/static/images/Facebook.png?raw=true" style="width: 100px; float: left;"/>\n\n' \
+#              '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#              'analysis/static/images/Pandas.png?raw=true" style="width: 400px; float: left;"/>'
+#
+#     imports = 'import pandas as pd\n' \
+#               'import matplotlib\n' \
+#               'import numpy as np\n' \
+#               'import calendar\n\n' \
+#               '%matplotlib inline'
+#
+#     number_header = '### 1. Total number of posts:'
+#     number_of_posts = 'results = pd.read_csv("analysis_csv_{}.csv", index_col=None)\n'.format(entity_id) + \
+#                       'number_of_posts = results["ID"].count()\n' \
+#                       'print("Total number of posts: {}".format(number_of_posts))'
+#
+#     popular_header = '### 2. Most popular posts:'
+#     popular_posts = 'def find_most(column_name):\n' \
+#                     '    most = results[["ID", column_name, "Author", "Date created", "Message"]]\n' \
+#                     '    most = most.sort_values(by=[column_name], ascending=False)[:5].reset_index(drop=True)\n' \
+#                     '    most.index = most.index + 1\n' \
+#                     '    return most'
+#
+#     reactions_subheader = '**a) Posts with the highest number of reactions:**\n\n' \
+#                           '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#                           'analysis/static/images/Reactions.png?raw=true" style="width: 400px; float: left;"/>'
+#     most_reactions = 'find_most("Number of reactions")'
+#
+#     likes_subheader = '**b) Posts with the highest number of likes:**\n\n' \
+#                       '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#                       'analysis/static/images/Like.png?raw=true" style="width: 400px; float: left;"/>'
+#     most_likes = 'find_most("Number of Likes")'
+#
+#     shares_subheader = '**c) Posts with the highest number of shares:**\n\n' \
+#                        '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#                        'analysis/static/images/Share.png?raw=true" style="width: 400px; float: left;"/>'
+#     most_shares = 'find_most("Number of shares")'
+#
+#     comments_subheader = '**d) Posts with the highest number of comments:**\n\n' \
+#                          '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#                          'analysis/static/images/Comment.png?raw=true" style="width: 400px; float: left;"/>'
+#     most_comments = 'find_most("Number of comments")'
+#
+#     unpopular_header = '### 2. Most unpopular posts:'
+#
+#     angrys_subheader = '**a) Posts with the highest number of ANGRY reactions:**\n\n' \
+#                        '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#                        'analysis/static/images/Angry.png?raw=true" style="width: 400px; float: left;"/>'
+#     most_angrys = 'find_most("Number of Angrys")'
+#
+#     sads_subheader = '**b) Posts with the highest number of SAD reactions:**\n\n' \
+#                      '<img src="https://github.com/IgorRosocha/FacebookPostsAnalysis/blob/master/' \
+#                      'analysis/static/images/Sad.png?raw=true" style="width: 400px; float: left;"/>'
+#     most_sads = 'find_most("Number of Sads")'
+#
+#     months_header = '### 3. Months with the biggest activity'
+#
+#     biggest_activity = 'activity = results[["Number of reactions", "Number of comments", "Date created"]]\n' \
+#                        'activity = activity.assign(Ratio=activity["Number of reactions"] + ' \
+#                        'activity["Number of comments"])\n' \
+#                        'activity["Date created"] = pd.to_datetime(activity["Date created"])\n' \
+#                        'activity = activity.sort_values(by="Date created")\n' \
+#                        'activity["mnth_yr"] = activity["Date created"].apply(lambda x: x.strftime("%b-%Y"))\n' \
+#                        'activity = activity.groupby(["mnth_yr"], sort=False).sum()\n' \
+#                        'activity = activity.drop(["Number of comments", "Number of reactions"], 1)'
+#
+#     graph = 'if activity["Ratio"].max() < 250:\n' \
+#             '    ticks = 10\n' \
+#             'elif activity["Ratio"].max() < 2500:\n' \
+#             '    ticks = 100\n' \
+#             'else:\n' \
+#             '    ticks = 1000\n\n' \
+#             'graph = activity.plot.bar(color="#8b9dc3", title="Months with the biggest activity", ' \
+#             'figsize=(15,5), legend=True)\n' \
+#             'graph.set_xlabel("Month")\n' \
+#             'graph.set_ylabel("Activity Ratio")\n' \
+#             'graph.yaxis.set_ticks(np.arange(0,activity["Ratio"].max(),ticks))\n' \
+#             'graph.set_facecolor("#f7f7f7")\n' \
+#             'graph.grid("on", which="major", axis="y")'
+#
+#     nb['cells'] = [nbf.v4.new_markdown_cell(header),
+#                    nbf.v4.new_markdown_cell(images),
+#                    nbf.v4.new_code_cell(imports),
+#                    nbf.v4.new_markdown_cell(number_header),
+#                    nbf.v4.new_code_cell(number_of_posts),
+#                    nbf.v4.new_markdown_cell(popular_header),
+#                    nbf.v4.new_code_cell(popular_posts),
+#                    nbf.v4.new_markdown_cell(reactions_subheader),
+#                    nbf.v4.new_code_cell(most_reactions),
+#                    nbf.v4.new_markdown_cell(likes_subheader),
+#                    nbf.v4.new_code_cell(most_likes),
+#                    nbf.v4.new_markdown_cell(shares_subheader),
+#                    nbf.v4.new_code_cell(most_shares),
+#                    nbf.v4.new_markdown_cell(comments_subheader),
+#                    nbf.v4.new_code_cell(most_comments),
+#                    nbf.v4.new_markdown_cell(unpopular_header),
+#                    nbf.v4.new_markdown_cell(angrys_subheader),
+#                    nbf.v4.new_code_cell(most_angrys),
+#                    nbf.v4.new_markdown_cell(sads_subheader),
+#                    nbf.v4.new_code_cell(most_sads),
+#                    nbf.v4.new_markdown_cell(months_header),
+#                    nbf.v4.new_code_cell(biggest_activity),
+#                    nbf.v4.new_code_cell(graph)]
+#
+#     notebook_name = 'analysis_' + entity_id + '.ipynb'
+#
+#     with open(notebook_name, 'w') as f:
+#         nbf.write(nb, f)
 
 
 def process_time(start_time):
@@ -460,6 +461,9 @@ def get_posts(ctx, entity, **configuration):
           'To see the results, open respective Notebook using Jupyter!\n'
           'For more information, please check the documentation or visit '
           'https://github.com/IgorRosocha/FacebookPostsAnalysis!')
+
+if __name__ == "__main__":
+    cli(obj={})
 
 
 def main():
--- docs/FacebookPostsAnalysis.rst
+++ docs/FacebookPostsAnalysis.rst
@@ -1,12 +1,20 @@
 API documentation
 ==================
 
-**API** consists of one main **analysis** module. Here you can see the full documentation of API methods and functions.
+**API** consists of two main modules: **analysis** module and **notebook** module. Here you can see the full documentation of API methods and functions.
 
 FacebookPostsAnalysis\.analysis module
 --------------------------------------
 
 .. automodule:: FacebookPostsAnalysis.analysis
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+FacebookPostsAnalysis\.notebook module
+--------------------------------------
+
+.. automodule:: FacebookPostsAnalysis.notebook
     :members:
     :undoc-members:
     :show-inheritance:
